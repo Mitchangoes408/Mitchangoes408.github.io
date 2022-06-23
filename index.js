07c753f9803b81aac719919fe213df0929c70853
Mitchangoes408 <54597899+Mitchangoes408@users.noreply.github.com>
2022-06-22 21:43:21 -0700

    npm init

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-<!DOCTYPE html>
-<html>
-  <body>
-
-    <h1>Hello World</h1>
-    <p>I'm hosted with GitHub Pages.</p>
-
-    
-  </body>
-</html>
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"))
